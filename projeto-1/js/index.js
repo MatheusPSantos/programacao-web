81116aef9271063f1a1f0faee85c269dd4cbfcab
matheuspsantos <msantos.2015@alunos.utfpr.edu.br>
2021-07-25 00:01:12 -0300

    funções para validação do formulário de login, iniciando request login

--- projeto-1/js/index.js
+++ projeto-1/js/index.js
@@ -1,12 +1,83 @@
-let loginButton = document.getElementById("btnlogin");
-let buttonCloseLogin = document.getElementById("close-login-box");
-let loginBox = document.getElementById("login-box");
+const loginButton = document.getElementById("btnlogin");
+const buttonCloseLogin = document.getElementById("close-login-box");
+const loginBox = document.getElementById("login-box");
+const loginForm = document.getElementById("login-form");
+const loginEmail = document.getElementById("email");
+const loginPass = document.getElementById("pass");
+const spanAlert = document.getElementById("display-alert");
+const alertMessage = "";
 
+const requestConfig = {
+    headers: {
+        "Content-Type": "application/json",
+    },
+    mode: "no-cors"
+};
+
+const loginAPI = "https://reqres.in/api/login";
 
 buttonCloseLogin.addEventListener('click', () => {
     loginBox.setAttribute("style", "display:none");
 });
 
 loginButton.addEventListener('click', () => {
-    loginBox.setAttribute("style", "display:inherent");
-})
+    loginBox.setAttribute("style", "display:inherit");
+});
+
+loginPass.addEventListener('change', (event) => {
+    event.preventDefault();
+    if (loginPass.classList.toString().includes("invalid-input")) {
+        loginPass.classList.remove("invalid-input");
+        spanAlert.innerText = "";
+    }
+});
+
+
+loginEmail.addEventListener('change', (event) => {
+    event.preventDefault();
+    if (loginEmail.classList.toString().includes("invalid-input")) {
+        loginEmail.classList.remove("invalid-input");
+        spanAlert.innerText = "";
+    }
+});
+
+loginForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    let valEmail = loginEmail.value;
+    let valPass = loginPass.value;
+
+    if (valEmail.length <= 3 || valPass.length <= 3) {
+        spanAlert.innerText = alertMessage;
+        loginPass.classList.add("invalid-input");
+        loginEmail.classList.add("invalid-input");
+        spanAlert.innerText = alertMessage;
+
+        return;
+    }
+
+    requestLogin(valEmail, valPass);
+});
+
+function requestLogin(user, pass) {
+    try {
+        console.log(user, pass)
+        let token = "";
+        let requestBody = {
+            email: user.trim(),
+            password: pass.trim()
+        };
+
+        fetch(loginAPI, {
+            method: "POST",
+            ...requestConfig,
+            body: JSON.stringify(requestBody)
+        }).then(response => {
+            console.log(response);
+        }).catch(err => {
+            console.error(err);
+        });
+    } catch (error) {
+        console.error('erro', error);
+    }
+}
