0fef91da5259f01c3f10abe2acdb61df3d6b079c
matheuspsantos <msantos.2015@alunos.utfpr.edu.br>
2021-08-25 10:04:11 -0300

    configurações do banco mongo

---
+++ projeto3/database/index.js
@@ -0,0 +1,29 @@
+const { MongoClient } = require('mongodb');
+const { mongoURL, dbName } = require('../config');
+
+const client = new MongoClient();
+
+async function connectMongo() {
+    try {
+        await client.connect(mongoURL,
+            { urlNewUrlParser: true }
+        );
+        console.info('Conexão com o banco realizada ...');
+        return client.db(dbName);
+    } catch (error) {
+        throw new Error(error);
+    }
+}
+
+async function closeMongo(db) {
+    try {
+        console.info('Fechando conexão com o mongodb ...');
+        db.close();
+        console.info('Conexão com o mangodb finalizada ...');
+        return;
+    } catch (error) {
+        throw new Error(error);
+    }
+}
+
+module.exports = { connectMongo, closeMongo };
