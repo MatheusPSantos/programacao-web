26b3d0feb9b913167ef13c16f802e8a207cb96a6
matheuspsantos <msantos.2015@alunos.utfpr.edu.br>
2021-08-26 10:20:34 -0300

    findOne e insertOne finalizados

---
+++ projeto3/models/User.js
@@ -0,0 +1,32 @@
+const database = require('../database/index');
+
+class User {
+    constructor() {
+        this.collection = 'users';
+    };
+
+    async connect() {
+        const db = await database.connectMongo();
+        return db.collection(this.collection);
+    }
+
+    async findOne(req) {
+        try {
+            const users = await this.connect();
+            return await users.findOne(req);
+        } catch (error) {
+            throw new Error(error);
+        }
+    }
+
+    async insertOne(req) {
+        try {
+            const users = await this.connect();
+            return users.insertOne(req);
+        } catch (error) {
+            throw new Error(error);
+        }
+    }
+}
+
+module.exports = User;
