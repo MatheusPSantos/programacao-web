1e71aeb18556892ea45c1ea8859b517effcc2e03
matheuspsantos <msantos.2015@alunos.utfpr.edu.br>
2021-08-26 10:19:53 -0300

    roteamento para os controllers basicos

--- projeto3/routes.js
+++ projeto3/routes.js
@@ -1,7 +1,13 @@
 const express = require('express');
 const routes = express.Router();
+const UserController = require('./controllers/UserController');
 
-routes.get('/user', (req,res) => console.log('rota de usuário'));
-routes.post('/user', (req,res) => console.log('rota de usuário'));
+
+routes.get('/user',
+    (request, response) => UserController.index(request, response)
+);
+routes.post('/user',
+    (request, response) => UserController.create(request, response)
+);
 
 module.exports = routes;
