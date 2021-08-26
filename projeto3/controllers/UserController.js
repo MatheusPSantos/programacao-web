d67ed80b961e02e372ab55cb6530f1a49e1d9958
matheuspsantos <msantos.2015@alunos.utfpr.edu.br>
2021-08-26 10:20:20 -0300

    função de inserir usuário finalizada

--- projeto3/controllers/UserController.js
+++ projeto3/controllers/UserController.js
@@ -1,3 +1,33 @@
-module.exports = {
-    
-};
+const database = require('../database/index');
+const User = require('../models/User');
+class UserController {
+    constructor() {
+        this.collectionName = 'users';
+        this.UserModel = new User();
+    }
+
+    async index() {
+        console.log('index')
+    }
+
+    async create(request, response) {
+        try {
+            const { body } = request;
+            let user = await this.UserModel.findOne({username: body.username});
+            if (!user) {
+                user = await this.UserModel.insertOne(body);
+                return response.json(user);
+                console.log('criar usuario')
+            } else {
+                return response.status(409).json({
+                    error: 'Já existe um usuário com esse username.'
+                });
+            }
+        } catch (error) {
+            new Error(error);
+        }
+    }
+
+}
+
+module.exports = new UserController();
