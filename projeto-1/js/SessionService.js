22ac5630be59ed3d4276ab471b2c23e75809232b
matheuspsantos <msantos.2015@alunos.utfpr.edu.br>
2021-08-28 19:54:16 -0300

    função de salvar e recuperar session feita

---
+++ projeto-1/js/SessionService.js
@@ -0,0 +1,16 @@
+function saveSession(data) {
+    const { sessionID, user } = data;
+    document.cookie = `session=${sessionID}`;
+    document.cookie = `username=${user.username}`;
+    document.cookit = `user_acl=${user.acl}`;
+}
+
+function getSession() {
+    let cookies = document.cookie.split(';');
+    let exists = cookies.filter(c => c.includes('session'));
+    if (exists.length !== 0) {
+        let val = exists[0].split("=")[1];
+        return val !== "" ? val : null;
+    }
+    return null;
+}
