d9198faa59a70e1e27c7d910cea1fbbc254a8c4f
Gustavo Kenji <gustavokomatsu4@gmail.com>
2021-08-27 21:13:52 -0300

    express-session config

--- projeto3/index.js
+++ projeto3/index.js
@@ -1,11 +1,22 @@
 const express = require('express');
+const session = require('express-session');
 const app = express();
 const routes = require('./routes');
 const PORT = 3000;
 
 //  middlewares
 app.use(express.json());
+app.use(session({
+	secret: 'projeto3',
+	resave: false,
+	saveUninitialized: true,
+	cookie: {secure: false},
+}));
 //  rotas
+app.get("/", ((req, res) => {
+	console.log("session ID >>>>>", req.sessionID);
+}));
+
 app.use(routes);
 
 app.listen(PORT);
