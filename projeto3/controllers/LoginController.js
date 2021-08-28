150cc61b14dca6bc39167ab5d2416b8cd4932234
Gustavo Kenji <gustavokomatsu4@gmail.com>
2021-08-27 21:50:08 -0300

    funcionalidade de logout criada

--- projeto3/controllers/LoginController.js
+++ projeto3/controllers/LoginController.js
@@ -2,38 +2,44 @@ const { compareSync } = require("bcryptjs");
 const User = require("../models/User");
 const Session = require("../models/Session");
 
-
 class LoginController {
-    constructor() {
-        this.UserModel = new User();
-        this.SessionModel = new Session();
-    }
+  constructor() {
+    this.UserModel = new User();
+    this.SessionModel = new Session();
+  }
 
-    async login(request, response) {
-			console.log(" request ID >>>", request.sessionID);
-			try {
-				const { body, sessionID } = request;
-				let user = await this.UserModel.findOne({ username: body.username });
-				if (user) {
-					if(compareSync(body.password, user.password)) {
-						let session = await this.SessionModel.createSession({username: body.username, session: sessionID});
-						return response.status(200).json({user, sessionID});
-					} else {
-						return response.status(401).json({ error: 'Não autorizado' });
-					}
-				} else {
-				    return response.status(400).json({ error: 'Usuário não existe' });
-				}
-					
-					return 0;
-				} catch (error) {
-					throw new Error(error);
-				}
-			}
-
-    logout() {
-        
+  async login(request, response) {
+    try {
+      const { body, sessionID } = request;
+      let user = await this.UserModel.findOne({ username: body.username });
+      if (user) {
+        if (compareSync(body.password, user.password)) {
+          let session = await this.SessionModel.createSession({
+            username: body.username,
+            session: sessionID,
+          });
+          return response.status(200).json({ user, sessionID });
+        } else {
+          return response.status(401).json({ error: "Não autorizado" });
+        }
+      } else {
+        return response.status(400).json({ error: "Usuário não existe" });
+      }
+    } catch (error) {
+      throw new Error(error);
     }
+  }
+
+  async logout(request, response) {
+		try {
+			const { body } = request;
+			const { sessionID } = body;
+			let deletedSession = await this.SessionModel.endSession({ session: sessionID });
+			return response.status(200).json( deletedSession );
+		} catch (error) {
+			throw new Error(error);
+		}
+	}
 }
 
 module.exports = new LoginController();
