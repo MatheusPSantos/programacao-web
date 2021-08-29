f06c31cfe5f8fbdd9fded757cfc4cf830015aef1
matheuspsantos <msantos.2015@alunos.utfpr.edu.br>
2021-08-29 10:54:16 -0300

    setando url do mongo

--- projeto3/config.js
+++ projeto3/config.js
@@ -1,4 +1,5 @@
 module.exports = {
     mongoURL: 'mongodb+srv://admin:projeto3@cluster0.220ie.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+    //mongoURL: 'mongodb://127.0.0.1:27017',
     dbName: 'projeto3'
 };
