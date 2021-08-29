b272d1bf8a891efe8af599a1949dc1c4a5087c93
matheuspsantos <msantos.2015@alunos.utfpr.edu.br>
2021-08-29 20:10:46 -0300

    atualizando alguns arquivos para rodar no heroku

--- projeto-1/js/configs.js
+++ projeto-1/js/configs.js
@@ -1,3 +1,2 @@
 const href = window.location.href;
-//const API_URL = href.includes("localhost") || href.includes('127.0.0.1') ? "http://localhost:3000" : "https://programacao-web-projeto-3.herokuapp.com/";
-const API_URL ="https://programacao-web-projeto-3.herokuapp.com";
+const API_URL = href.includes("localhost") || href.includes('127.0.0.1') ? "http://localhost:3000" : "https://progweb-proj3.herokuapp.com";
