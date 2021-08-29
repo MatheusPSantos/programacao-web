5b73deeb97ae4e5889c0102fc6945479e0c4c540
matheuspsantos <msantos.2015@alunos.utfpr.edu.br>
2021-08-29 18:11:04 -0300

    set node nev prod

--- projeto-1/js/configs.js
+++ projeto-1/js/configs.js
@@ -1,2 +1,3 @@
 const href = window.location.href;
-const API_URL = href.includes("localhost") || href.includes('127.0.0.1') ? "http://localhost:3000" : "";
+//const API_URL = href.includes("localhost") || href.includes('127.0.0.1') ? "http://localhost:3000" : "https://programacao-web-projeto-3.herokuapp.com/";
+const API_URL ="https://programacao-web-projeto-3.herokuapp.com";
